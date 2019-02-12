85cdb538eac404a3e485139c8d85072bb87fafab
Stepan <kurakste@gmail.com>
2019-02-12 23:20:07 +0300

    decatlon

--- bots/allcardshere/public/bot.php
+++ bots/allcardshere/public/bot.php
@@ -100,6 +100,19 @@ try {
                     ->setKeyboard($kbrd)
             );
         })
+        
+        ->onText("/decatlon|декатлон/ius", function ($event) use ($bot, $botSender, $log, $storage) {
+            $log->info('onClear' . $event->getMessage()->getText());
+            $kbrd = require_once(__DIR__.'/../keyboards/mainMenu.php');
+            $bot->getClient()->sendMessage(
+                (new \Viber\Api\Message\Picture())
+                    ->setSender($botSender)
+                    ->setReceiver($event->getSender()->getId())
+                    ->setText('Дайте отсканировать эту карту кассиру')
+                    ->setMedia('https://'.$_SERVER['SERVER_NAME'].'/bots/allcardshere/img/cards/decatlon.jpeg')
+                    ->setKeyboard($kbrd)
+            );
+        })
 
         ->onText('|.*|s', function ($event) use ($bot, $botSender, $log, $storage) {
             $storage->logMessageToDb(
