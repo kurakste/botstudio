f5fb331ed91f359a4d3288943976b3bcd3fb7759
Stepan <kurakste@gmail.com>
2019-02-12 23:37:03 +0300

    polushka

--- bots/allcardshere/public/bot.php
+++ bots/allcardshere/public/bot.php
@@ -192,6 +192,19 @@ try {
             );
         })
 
+        ->onText("/polushka|полушка/ius", function ($event) use ($bot, $botSender, $log, $storage) {
+            $log->info('onClear' . $event->getMessage()->getText());
+            $kbrd = require_once(__DIR__.'/../keyboards/mainMenu.php');
+            $bot->getClient()->sendMessage(
+                (new \Viber\Api\Message\Picture())
+                    ->setSender($botSender)
+                    ->setReceiver($event->getSender()->getId())
+                    ->setText('Дайте отсканировать эту карту кассиру')
+                    ->setMedia('https://'.$_SERVER['SERVER_NAME'].'/bots/allcardshere/img/cards/polushka.jpeg')
+                    ->setKeyboard($kbrd)
+            );
+        })
+
         ->onText('|.*|s', function ($event) use ($bot, $botSender, $log, $storage) {
             $storage->logMessageToDb(
                 "allcardshere",
