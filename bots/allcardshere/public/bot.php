73031c4474726595422dbf168cb8188e60492082
Stepan <kurakste@gmail.com>
2019-02-12 23:33:49 +0300

    okey

--- bots/allcardshere/public/bot.php
+++ bots/allcardshere/public/bot.php
@@ -178,6 +178,18 @@ try {
                     ->setKeyboard($kbrd)
             );
         })
+        
+        ->onText("/okey|окей/ius", function ($event) use ($bot, $botSender, $log, $storage) {
+            $log->info('onClear' . $event->getMessage()->getText());
+            $kbrd = require_once(__DIR__.'/../keyboards/mainMenu.php');
+            $bot->getClient()->sendMessage(
+                (new \Viber\Api\Message\Picture())
+                    ->setSender($botSender)
+                    ->setReceiver($event->getSender()->getId())
+                    ->setText('Дайте отсканировать эту карту кассиру')
+                    ->setMedia('https://'.$_SERVER['SERVER_NAME'].'/bots/allcardshere/img/cards/okey.jpeg')
+                    ->setKeyboard($kbrd)
+            );
 
         ->onText('|.*|s', function ($event) use ($bot, $botSender, $log, $storage) {
             $storage->logMessageToDb(
