2a0b717e707294c31a48643b08c7903ddf45ea76
Stepan <kurakste@gmail.com>
2019-02-12 23:32:12 +0300

    obi

--- bots/allcardshere/public/bot.php
+++ bots/allcardshere/public/bot.php
@@ -166,6 +166,19 @@ try {
             );
         })
 
+        ->onText("/obi|оби/ius", function ($event) use ($bot, $botSender, $log, $storage) {
+            $log->info('onClear' . $event->getMessage()->getText());
+            $kbrd = require_once(__DIR__.'/../keyboards/mainMenu.php');
+            $bot->getClient()->sendMessage(
+                (new \Viber\Api\Message\Picture())
+                    ->setSender($botSender)
+                    ->setReceiver($event->getSender()->getId())
+                    ->setText('Дайте отсканировать эту карту кассиру')
+                    ->setMedia('https://'.$_SERVER['SERVER_NAME'].'/bots/allcardshere/img/cards/obi.jpeg')
+                    ->setKeyboard($kbrd)
+            );
+        })
+
         ->onText('|.*|s', function ($event) use ($bot, $botSender, $log, $storage) {
             $storage->logMessageToDb(
                 "allcardshere",
